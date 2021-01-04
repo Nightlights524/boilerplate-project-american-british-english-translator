06d980349ce05cfba262eb4ffb93f11f679255b3
Nightlights524 <Nightlights524@users.noreply.github.com>
2021-01-03 23:28:47 -0600

    All 6 Functional Tests passing

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,176 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Translator = require('../components/translator.js');
+const translator = new Translator();
 
 suite('Unit Tests', () => {
+
+  // test('Translate Mangoes are my favorite fruit.', done => {
+  //   const text = 'Translate Mangoes are my favorite fruit.';
+  //   const translation = translator.translate(text, 'american-to-british');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'american-to-british');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'american-to-british');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'american-to-british');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'american-to-british');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'american-to-british');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'american-to-british');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'american-to-british');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'american-to-british');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'american-to-british');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'british-to-american');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'british-to-american');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'british-to-american');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'british-to-american');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'british-to-american');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'british-to-american');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'british-to-american');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'british-to-american');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'british-to-american');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'british-to-american');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'american-to-british');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'american-to-british');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'british-to-american');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
+
+  // test('', done => {
+  //   const text = '';
+  //   const translation = translator.translate(text, 'british-to-american');
+  //   assert.deepEqual({text, translation});
+  //   done();
+  // });
 
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,4 +9,82 @@ let Translator = require('../components/translator.js');
 
 suite('Functional Tests', () => {
 
+  test('Translation with text and locale fields', done => {
+    const text = 'Mangoes are my favorite fruit.';
+    const translation = 'Mangoes are my <span class=\"highlight\">favourite</span> fruit.';
+    const locale = 'american-to-british';
+    chai.request(server)
+      .post('/api/translate')
+      .send({text, locale})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {text, translation});
+        done();
+      });
+  });
+
+  test('Translation with text and invalid locale field', done => {
+    const text = 'Mangoes are my favorite fruit.';
+    const locale = 'americaaaan-to-british';
+    chai.request(server)
+      .post('/api/translate')
+      .send({text, locale})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {error: 'Invalid value for locale field'});
+        done();
+      });
+  });
+
+  test('Translation with missing text field', done => {
+    const locale = 'american-to-british';
+    chai.request(server)
+      .post('/api/translate')
+      .send({locale})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {error: 'Required field(s) missing'});
+        done();
+      });
+  });
+
+  test('Translation with missing locale field', done => {
+    const text = 'Mangoes are my favorite fruit.';
+    chai.request(server)
+      .post('/api/translate')
+      .send({text})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {error: 'Required field(s) missing'});
+        done();
+      });
+  });
+
+  test('Translation with empty text', done => {
+    const text = '';
+    const locale = 'american-to-british';
+    chai.request(server)
+      .post('/api/translate')
+      .send({text, locale})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {error: 'No text to translate'});
+        done();
+      });
+  });
+
+  test('Translation with text that needs no translation', done => {
+    const text = 'Mangoes are my favourite fruit.';
+    const translation = 'Everything looks good to me!';
+    const locale = 'american-to-british';
+    chai.request(server)
+      .post('/api/translate')
+      .send({text, locale})
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.deepEqual(res.body, {text, translation});
+        done();
+      });
+  });
+
 });
